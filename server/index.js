1cc497ca3bc0a431c8ede4918ec922360672d4b7
alinabezux <alinabezux@gmail.com>
2024-04-27 12:27:34 +0300

    commit2

--- server/index.js
+++ server/index.js
@@ -6,11 +6,12 @@ const router = require('./routes/index');
 
 const ApiError = require('./errors/ApiError');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+
 app.use('/api', router);
 
 app.get('/', (req, res) => {
