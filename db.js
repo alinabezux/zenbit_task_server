5700f3db75847915f4e8b43291be42968b5a36ad
alinabezux <alinabezux@gmail.com>
2024-04-27 16:35:42 +0300

    fefegreg

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 const {Sequelize} = require('sequelize')
+const pg = require("pg");
 require('dotenv').config();
 
-module.exports = new Sequelize(process.env.POSTGRES_URL)
+module.exports = new Sequelize(process.env.POSTGRES_URL, {dialectModule: pg})
+
